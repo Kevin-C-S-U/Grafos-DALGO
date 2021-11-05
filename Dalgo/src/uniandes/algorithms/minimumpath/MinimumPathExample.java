6cb48b5fee4a61428a87ef542c10b4234bb23c84
SantiagoPardo726 <69635343+SantiagoPardo726@users.noreply.github.com>
2021-11-05 15:53:50 -0500

    Add files via upload

---
+++ Dalgo/src/uniandes/algorithms/minimumpath/MinimumPathExample.java
@@ -0,0 +1,13 @@
+package uniandes.algorithms.minimumpath;
+
+public class MinimumPathExample {
+
+	public static void main(String[] args) {
+		String Filename = args[0];
+		String algorithm = null;
+		if(args.length>1) {
+			algorithm = args[1];
+		}
+		
+	}
+}
---
+++ Dalgo/src/uniandes/algorithms/minimumpath/UndirectedGraph.java
@@ -0,0 +1,61 @@
+package uniandes.algorithms.minimumpath;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UndirectedGraph {
+	
+	private List<Integer> vertex;
+	
+	private List<ArrayList<Integer>> edges;
+	
+	private List<Integer> weights;
+	
+	public UndirectedGraph() {
+		
+		this.vertex = new ArrayList<Integer>();
+		
+		this.edges = new ArrayList<ArrayList<Integer>>();
+		
+		this.weights = new ArrayList<Integer>();
+	}
+	
+	/**
+	 * 
+	 * @param idFirst
+	 * @param idSecond
+	 * @param weight
+	 */
+	public void addEdges(int idFirst,int idSecond,int weight) {
+		if(weight!=-1) {
+			ArrayList<Integer> path = new ArrayList<Integer>();
+			path.add(idFirst);
+			path.add(idSecond);
+			this.edges.add(path);
+			this.weights.add(weight);
+			if(!vertex.contains(idFirst)) {
+				this.vertex.add(idFirst);
+			}
+			if(!vertex.contains(idSecond)){
+				this.vertex.add(idFirst);
+			}
+		}else {
+			if(!vertex.contains(idFirst)) {
+				this.vertex.add(idFirst);
+			}
+			if(!vertex.contains(idSecond)){
+				this.vertex.add(idFirst);
+			}
+		}
+	}
+	public void getWeight(int idFirst,int idSecond) {
+		int pos = -1;
+		for(ArrayList<Integer> path : this.edges) {
+			if(path.get(0)==idFirst && path.get(1)==idSecond){
+				
+			}
+		}
+	}
+	
+
+}
