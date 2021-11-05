a7a4259c333e3673b1c94e557fba7d7869b50c97
Santiago Pardo <s.pardob@uniandes.edu.co>
2021-11-05 16:20:55 -0500

    getweight

--- Dalgo/src/uniandes/algorithms/minimumpath/UndirectedGraph.java
+++ Dalgo/src/uniandes/algorithms/minimumpath/UndirectedGraph.java
@@ -4,59 +4,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UndirectedGraph {
-	
+
 	private List<Integer> vertex;
-	
+
 	private List<ArrayList<Integer>> edges;
-	
+
 	private List<Integer> weights;
-	
+
 	public UndirectedGraph() {
-		
+
 		this.vertex = new ArrayList<Integer>();
-		
+
 		this.edges = new ArrayList<ArrayList<Integer>>();
-		
+
 		this.weights = new ArrayList<Integer>();
 	}
-	
+
 	/**
 	 * 
 	 * @param idFirst
 	 * @param idSecond
 	 * @param weight
 	 */
-	public void addEdges(int idFirst,int idSecond,int weight) {
-		if(weight!=-1) {
+	public void addEdges(int idFirst, int idSecond, int weight) {
+		if (weight != -1) {
 			ArrayList<Integer> path = new ArrayList<Integer>();
 			path.add(idFirst);
 			path.add(idSecond);
 			this.edges.add(path);
 			this.weights.add(weight);
-			if(!vertex.contains(idFirst)) {
+			if (!vertex.contains(idFirst)) {
 				this.vertex.add(idFirst);
 			}
-			if(!vertex.contains(idSecond)){
+			if (!vertex.contains(idSecond)) {
+				this.vertex.add(idSecond);
+			}
+		} else {
+			if (!vertex.contains(idFirst)) {
 				this.vertex.add(idFirst);
 			}
-		}else {
-			if(!vertex.contains(idFirst)) {
-				this.vertex.add(idFirst);
-			}
-			if(!vertex.contains(idSecond)){
+			if (!vertex.contains(idSecond)) {
 				this.vertex.add(idFirst);
 			}
 		}
 	}
-	public void getWeight(int idFirst,int idSecond) {
+
+	public int getWeightPosition(int idFirst, int idSecond) throws Exception {
 		int pos = 0;
 		boolean found = false;
-		for(ArrayList<Integer> path : this.edges) {
-			if(path.get(0)==idFirst && path.get(1)==idSecond){
-				pos ++;
+		for (ArrayList<Integer> path : this.edges) {
+			if (path.get(0) == idFirst && path.get(1) == idSecond) {
+				found = true;
+				break;
 			}
+			pos++;
 		}
+		if (found == false) {
+			throw new Exception();
+		}
+		return pos;
 	}
-	
 
 }
