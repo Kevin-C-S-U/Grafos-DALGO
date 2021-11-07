e967f5f3c722b716af54502436cdb8fdca60f828
Kevin-C-S <kevincohensolano@gmail.com>
2021-11-07 17:52:42 -0500

    Correcciones

--- Dalgo/src/uniandes/algorithms/minimumpath/GraphMinimumPath.java
+++ Dalgo/src/uniandes/algorithms/minimumpath/GraphMinimumPath.java
@@ -4,5 +4,5 @@ import java.util.ArrayList;
 
 public interface GraphMinimumPath {
 
-	public ArrayList<Integer> minimumpath(DirectedGraph graph,int idFirst);
+	public int[][] minimumpath(DirectedGraph graph);
 }
