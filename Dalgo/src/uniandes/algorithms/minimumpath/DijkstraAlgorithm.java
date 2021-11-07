c7d20e7ed77e1de0f0f200b6c5594fd7b5f1ae8b
Kevin-C-S <kevincohensolano@gmail.com>
2021-11-07 16:52:43 -0500

    Dijkstra

---
+++ Dalgo/src/uniandes/algorithms/minimumpath/DijkstraAlgorithm.java
@@ -0,0 +1,49 @@
+package uniandes.algorithms.minimumpath;
+
+import java.util.Arrays;
+
+public class DijkstraAlgorithm {
+	
+	public int[][] DijkstraCompleto(int[][] grafo){
+		int[][] minCost = new int[grafo.length][grafo[0].length];
+		for (int i = 0;i<grafo.length;i++) {
+			minCost[i] = Dijkstra(grafo,i);
+		}
+		return minCost;
+	}
+	
+	public int[] Dijkstra(int[][] grafo, int vertice) {
+		//Iniciamos el arreglo de respuesta y lo llenamos de los arcos directos
+		int[] minCost = new int[grafo.length];
+		minCost = grafo[vertice];
+		boolean[] marcados = new boolean[grafo.length];
+		
+		//Iniciamos la lista de marcados con false y true en el vertice
+		for(int m = 0;m < grafo.length;m++) {
+			if (m==vertice) {
+				marcados[m]=true;
+			}else {
+				marcados[m] = false;
+			}
+		}
+		int ver = 0;
+		while (Arrays.asList(marcados).contains(false)) {
+			int control = 100000000;
+			for (int i = 0;i<grafo.length;i++) {
+				if (minCost[i]<control && marcados[i] == false && minCost[i] != -1) {
+					ver = i;
+					control = minCost[i];
+				}
+			}
+			marcados[ver] = true;
+			for (int j = 0;j<grafo.length;j++) {
+				if (minCost[j]>minCost[ver]+grafo[ver][j]) {
+					minCost[j] = minCost[ver]+grafo[ver][j];
+				}
+			}
+		}
+		
+		return minCost;
+	}
+
+}
